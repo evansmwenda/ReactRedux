a94eb92db34cf77e9d6223ef0495cf50cdd2f6dc
evansmwenda <evansmwenda.em@gmail.com>
2019-11-05 14:43:45 +0300

    redux demo complete

--- App.js
+++ App.js
@@ -24,10 +24,16 @@ const initialState = {
   counter:0,//initial state of the variable counter
 }
 
-const reducer = (state=initialState) =>{
+const reducer = (state=initialState,action) =>{
   //this is a reducer
   //takes previous state and an action as arguement
   // and returns a new state
+  switch(action.type){
+    case 'INCREASE_VALUE':
+      return {counter:state.counter+1}
+    case 'DECREASE_VALUE':
+        return {counter:state.counter-1}
+  }
   return state
 }
 
--- src/CounterApp.js
+++ src/CounterApp.js
@@ -17,36 +17,20 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {connect} from 'react-redux'
+
 
 class CounterApp extends Component {
-  state={
-    counter:0
-  }
-
-  increaseCounter = () => {
-    this.setState({counter:this.state.counter+1})
-  }
-
-  decreaseCounter = () => {
-    this.setState({counter:this.state.counter-1})
-  }
 
   render(){
     return (
       <View style={styles.mainContainer}>
         <View style={{flexDirection:'row',width:200,justifyContent:'space-around'}}>
-          <TouchableOpacity onPress={()=> this.increaseCounter()}>
+          <TouchableOpacity onPress={()=> this.props.increaseCounter()}>
             <Text style={{fontSize:20}}>Increase</Text>
           </TouchableOpacity>
-          <Text style={{fontSize:18}}>{this.state.counter}</Text>
-          <TouchableOpacity onPress={()=> this.decreaseCounter()}>
+          <Text style={{fontSize:18}}>{this.props.counter}</Text>
+          <TouchableOpacity onPress={()=> this.props.decreaseCounter()}>
             <Text style={{fontSize:20}}>Decrease</Text>
           </TouchableOpacity>
         </View>
@@ -65,4 +49,17 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CounterApp;
+
+function mapStateToProps(state){
+    return{
+        counter:state.counter
+    }
+}
+function mapDispatchToProps(dispatch){
+    return {
+        increaseCounter: () => dispatch({type:'INCREASE_VALUE'}),
+        decreaseCounter: () => dispatch({type:'DECREASE_VALUE'}),
+
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(CounterApp);
